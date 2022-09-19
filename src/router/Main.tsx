25ba6d9c998a2f5059f7baff3befd207e5d54d3f
Benjenkins126 <benjenk@hotmail.co.uk>
2022-09-19 10:40:58 +0100

    Added Router

---
+++ src/router/Main.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+// import Page from '../pages/Page';
+
+const Main = () => {
+    return (
+        <Routes>
+             {/* <Route path='/page' element={<Page />} />*/}
+        </Routes>
+    );
+}
+
+export default Main;
