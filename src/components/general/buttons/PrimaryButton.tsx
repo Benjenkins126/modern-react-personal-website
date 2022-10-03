6bd1f6991c11ffe7604c3d6abcc29ff44ae82eb7
Benjenkins126 <benjenk@hotmail.co.uk>
2022-10-03 19:15:35 +0100

    Added hover effects to buttons

--- src/components/general/buttons/PrimaryButton.tsx
+++ src/components/general/buttons/PrimaryButton.tsx
@@ -10,7 +10,7 @@ const PrimaryButton = (props: PrimaryButtonProps) => {
     if(props.url) {
         return (
             <Link to={`${props.url}`}>
-                <button className={`h-11 bg-gradient-to-r from-primary to-secondary w-32 rounded-md font-ben text-white font-bold text-center text-sm ${props.classes}`}>
+                <button className={`transition-all duration-300 hover:opacity-75 ease-in-out h-11 bg-gradient-to-r from-primary to-secondary w-32 rounded-md font-ben text-white font-bold text-center text-sm  ${props.classes}`}>
                     {props.name}
                 </button>
             </Link>
--- src/components/general/buttons/SecondaryButton.tsx
+++ src/components/general/buttons/SecondaryButton.tsx
@@ -11,7 +11,7 @@ const SecondaryButton = (props: SecondaryButtonProps) => {
         return (
             <Link to={`${props.url}`}>
                 <button className={`h-11 bg-gradient-to-r from-primary to-secondary w-32 rounded-md font-ben text-white font-bold text-center text-sm ${props.classes}`}>
-                    <span className="bg-github-section w-124 h-40 leading-40 m-auto block rounded-md">{props.name}</span>
+                    <span className="bg-github-section w-124 h-40 leading-40 m-auto block rounded-md transition-all ease-in-out duration-300 hover:bg-transparent hover:text-white">{props.name}</span>
                 </button>
             </Link>
         );
